3a5a1b892ddeb991390a62d22a4cf9de53131f4a
brett.reynolds-james <brett.reynolds-james@lynqmes.com>
2023-03-07 11:39:13 +0000

    most recent

--- space.py
+++ space.py
@@ -1,21 +1,27 @@
 import requests
 import json
+import pandas as pd
 
 # apt-get install python python-dev python-pip virtualenvwrapper redis-server
 
 space = requests.get('http://api.open-notify.org/astros.json')
 
 space_dict = space.json()
+print(["message"])
 
 good = json.dumps(space_dict, indent= 4)
 print(good)
 
-#i hope
+x = space_dict["people"]
 
 
-
-
-
+print("number of people in space: ", space_dict["number"])
+print("\npeople currently in space:")
+print("craft                         name")
+x = 0
+while x < 15:
+    print(space_dict["people"])
+    x+1
 
 
 
